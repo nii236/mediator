e4fb7876491232f992a52064cffaaed7c58ae7e3
John Nguyen <jnguyen@thefrontiergroup.com.au>
2018-01-28 13:56:53 +0800

    working basic chat ws roundtrip

--- client/index.js
+++ client/index.js
@@ -5,46 +5,123 @@ Vue.use(VueNativeSock.default, "ws://localhost:9090", {
 	reconnectionDelay: 3000
 })
 
-var vm = new Vue({
+var messages = [
+	{
+		username: "nii236",
+		timestamp: "5 minutes ago",
+		message: "Chat has started",
+		isAlert: true
+	}
+]
+
+const vm = new Vue({
 	el: "#ws",
 	created: function() {
 		this.$options.sockets.onmessage = msg => {
-			const list = app7.groceryList
-			const item = {
-				id: list.length,
-				text: msg.data
+			const resp = JSON.parse(msg.data)
+			const messages = chatApp.messages
+			messages.push(resp)
+		}
+	}
+})
+
+const ChatBox = Vue.component("chat-box", {
+	props: ["meta", "messages"],
+	mounted: function() {
+		console.log("Mounted chatbox")
+		console.log(this.$props)
+	},
+
+	template: `
+    <div id="ChatBox">
+        <div class="col-md-9 ChatBox__Left">
+			<div class="ChatBox__List">
+				<chat-message v-for="message in messages" :data="message">
+				</chat-message>
+            </div>
+
+            <div class="ChatBox__Input">
+				<chat-input></chat-input>
+            </div>
+        </div>
+
+        <div class="col-md-3 ChatBox__Right">
+            <h3>Online Users</h3>
+
+            <ul class="ChatBox__OnlineUsers">
+				<li v-for="user in meta.onlineUsers">
+                    {{ user }}
+                </li>
+            </ul>
+        </div>
+    </div>
+`
+})
+
+const ChatMessage = Vue.component("chat-message", {
+	props: ["data"],
+	template: `
+<div class="Message">
+	<div class="Message--Alert" v-if="data.isAlert">
+		<strong>{{ data.message }}</strong>
+	</div>
+	<div class="Message--Message" v-else>
+		<p class="Message__Author">
+			<strong>{{ data.username }}</strong>: {{ data.message }}
+		</p>
+	</div>
+</div>`
+})
+
+var msg = ""
+
+const ChatInputInitialState = function() {
+	return { message: { text: "" } }
+}
+
+const ChatInput = Vue.component("chat-input", {
+	data: function() {
+		return ChatInputInitialState()
+	},
+
+	methods: {
+		handleSubmit() {
+			console.log(this.$data.message.text)
+			const payload = {
+				username: "clientperson",
+				timestamp: "5 minutes ago",
+				message: this.$data.message.text,
+				isAlert: false
 			}
-			app7.groceryList.push(item)
+			this.$socket.sendObj(payload)
+			Object.assign(this.$data, ChatInputInitialState())
 		}
 	},
-	methods: {
-		test: function(val) {
-			this.$socket.sendObj({ awesome: "data" })
-		}
-	}
+	template: `
+<form @submit.prevent="handleSubmit">
+	<input v-model="message.text" type="text"  placeholder="Enter your message here">
+</form>`
 })
 
-// this.$options.sockets.onmessage = data => console.log(data)
-
-var app = new Vue({
-	el: "#app",
+const chatApp = new Vue({
+	el: "#chat",
 	data: {
-		message: "Hello Vue!"
-	}
-})
-
-Vue.component("todo-item", {
-	props: ["todo"],
-	template: "<li>{{ todo.text }}</li>"
-})
-
-var app7 = new Vue({
-	el: "#app-7",
-	data: {
-		groceryList: [
-			{ id: 0, text: "Vegetables" },
-			{ id: 1, text: "Cheese" },
-			{ id: 2, text: "Whatever else humans are supposed to eat" }
+		meta: {
+			onlineUsers: ["nii236"]
+		},
+		messages: [
+			{
+				username: "nii236",
+				timestamp: "5 minutes ago",
+				message: "Chat has started",
+				isAlert: true
+			},
+			{
+				username: "nii236",
+				timestamp: "2 minutes ago",
+				message: "Hi there!",
+				isAlert: false
+			}
 		]
 	}
 })
